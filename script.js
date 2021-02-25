da086734d1c9501fa4cda995d0b565be8ba02905
Emeka Ike <70453914+EmekaIke@users.noreply.github.com>
2021-02-25 10:38:58 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>samplehair.com</title>
+	<style>
+body {
+  background-image: url(hair4.jpg); background-repeat: no-repeat; background-size: cover; background-attachment: fixed;
+}
+</style>
+</head>
+<body>beauty world is the home of fine collections for outings.  
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+<iframe src="border.html" title="my iframe example" height="200" width="300" align="center"></iframe>
+<ul>
+	<li>apple</li>
+	<ul>
+		<li>fuji</li>
+		<li>crest</li>
+	</ul>
+	<li>orange</li>
+	<li>guava</li>
+</ul>
+</body>
+</html>
